183ae4e28c0b96ee871e4a46879156aaa4b7670a
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2022-09-29 14:05:50 -0500

    Refactored back to ESBuild

--- index.js
+++ index.js
@@ -1,50 +1,21 @@
-// import fs from 'fs'
-// import core from '@actions/core'
-// import github from '@actions/github'
-// import esbuild from 'esbuild-wasm'
-// const fs = require('fs')
-const core = require('@actions/core')
-const github = require('@actions/github')
-// const esbuild = require('esbuild-wasm')
-const swc = require("@swc/core");
+import fs from 'fs'
+import core from '@actions/core'
+import github from '@actions/github'
+import esbuild from 'esbuild'
+import httpPlugin from 'esbuild-plugin-http'
 
-const run = async () => {
-  try {
-    const time = (new Date()).toTimeString()
-  //   const config = yaml.parse('_config.yaml')
-    const payload = JSON.stringify(github.context , null, 2)
-    // const ts = fs.readFileSync('worker.js')
+try {
+  const time = (new Date()).toTimeString()
 
-    // const ts = 'let test: boolean = true'
-    // const result = await esbuild.transform(ts, { loader: 'ts' })
-
-    
-
-  const result = await swc.transform("source code", {
-      // Some options cannot be specified in .swcrc
-      filename: "worker.js",
-      sourceMaps: true,
-      // Input files are treated as module by default.
-      isModule: true,
-
-      // All options below can be configured via .swcrc
-      jsc: {
-        parser: {
-          syntax: "ecmascript",
-        },
-        transform: {},
-      },
-    })
-    .then((output) => {
-      output.code; // transformed code
-      output.map; // source map (in string)
-    });
-
-    console.log('result:', result)
-    console.log(`The generated api.json: ${payload}`)
-  } catch (error) {
-    core.setFailed(error.stack)
-  }
+  const payload = JSON.stringify(github.context , null, 2)
+  esbuild.build({
+    entryPoints: ['./worker.js'],
+    bundle: true,
+    outfile: './dist.js',
+    plugins: [httpPlugin],
+  }).catch(() => process.exit(1))
+  
+  console.log(`The generated api.json: ${payload}`)
+} catch (error) {
+  core.setFailed(error.stack)
 }
-
-run()
