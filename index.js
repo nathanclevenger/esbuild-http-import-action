6861c8a812bb98ad9e31a6df5de1a6bc8acc0127
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2022-09-29 13:30:10 -0500

    switched to swc

--- index.js
+++ index.js
@@ -5,18 +5,41 @@
 // const fs = require('fs')
 const core = require('@actions/core')
 const github = require('@actions/github')
-const esbuild = require('esbuild-wasm')
+// const esbuild = require('esbuild-wasm')
+const swc = require("@swc/core");
 
 const run = async () => {
   try {
     const time = (new Date()).toTimeString()
   //   const config = yaml.parse('_config.yaml')
     const payload = JSON.stringify(github.context , null, 2)
-  //   fs.writeFileSync('api.json', payload)
-
     // const ts = fs.readFileSync('worker.js')
-    const ts = 'let test: boolean = true'
-    const result = await esbuild.transform(ts, { loader: 'ts' })
+
+    // const ts = 'let test: boolean = true'
+    // const result = await esbuild.transform(ts, { loader: 'ts' })
+
+    
+
+  const result = swc.transform("source code", {
+      // Some options cannot be specified in .swcrc
+      filename: "worker.js",
+      sourceMaps: true,
+      // Input files are treated as module by default.
+      isModule: true,
+
+      // All options below can be configured via .swcrc
+      jsc: {
+        parser: {
+          syntax: "ecmascript",
+        },
+        transform: {},
+      },
+    })
+    .then((output) => {
+      output.code; // transformed code
+      output.map; // source map (in string)
+    });
+
     console.log('result:', result)
     console.log(`The generated api.json: ${payload}`)
   } catch (error) {
