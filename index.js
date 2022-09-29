b016dd8fdb0b3f9beafd76555da46afc3922d42d
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2022-09-29 14:30:00 -0500

    Added dynamic inputs

--- index.js
+++ index.js
@@ -9,13 +9,16 @@ try {
 
   const payload = JSON.stringify(github.context , null, 2)
   esbuild.build({
-    entryPoints: ['./worker.js'],
-    bundle: true,
-    outfile: './dist.js',
+    entryPoints: [core.getInput('entryPoint')],
+    bundle: core.getInput('bundle'),
+    outfile: core.getInput('outfile'),
     plugins: [httpPlugin],
   }).catch(() => process.exit(1))
+
+  const output = fs.readFileSync(core.getInput('bundle'))
+  core.setOutput("bundle", output)
   
-  console.log(`The generated api.json: ${payload}`)
+  console.log(`The ESBuild output: ${output}`)
 } catch (error) {
   core.setFailed(error.stack)
 }
