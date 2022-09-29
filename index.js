bf8555a8bed5b954b0f701c2f5a8a71331b3ecde
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2022-09-29 13:11:07 -0500

    Trying out esbuild-wasm

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 import fs from 'fs'
 import core from '@actions/core'
 import github from '@actions/github'
-import yaml from 'yaml'
+import esbuild from 'esbuild-wasm'
 
 try {
   const time = (new Date()).toTimeString()
 //   const config = yaml.parse('_config.yaml')
   const payload = JSON.stringify({ github }, null, 2)
 //   fs.writeFileSync('api.json', payload)
-  const body = fs.readFileSync('worker.js')
+
+  // const ts = fs.readFileSync('worker.js')
+  const ts = 'let test: boolean = true'
+  const result = await esbuild.transform(ts, { loader: 'ts' })
+  console.log('result:', result)
   console.log(`The generated api.json: ${payload}`)
 } catch (error) {
   core.setFailed(error.message)
